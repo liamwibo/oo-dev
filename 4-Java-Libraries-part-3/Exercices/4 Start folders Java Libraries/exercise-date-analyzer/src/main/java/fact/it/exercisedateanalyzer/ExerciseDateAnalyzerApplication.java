6f7734dc601d2fa67c1bde575415697f0a0cf50c
liam <r1082482@student.thomasmore.be>
2026-03-02 18:31:29 +0100

    oefeningen api 3

--- 4-Java-Libraries-part-3/Exercices/4 Start folders Java Libraries/exercise-date-analyzer/src/main/java/fact/it/exercisedateanalyzer/ExerciseDateAnalyzerApplication.java
+++ 4-Java-Libraries-part-3/Exercices/4 Start folders Java Libraries/exercise-date-analyzer/src/main/java/fact/it/exercisedateanalyzer/ExerciseDateAnalyzerApplication.java
@@ -1,5 +1,6 @@
 package fact.it.exercisedateanalyzer;
 
+import fact.it.exercisedateanalyzer.model.DateAnalyzer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +11,21 @@ public class ExerciseDateAnalyzerApplication {
 
         SpringApplication.run(ExerciseDateAnalyzerApplication.class, args);
 
+        DateAnalyzer myDate = new DateAnalyzer("15/06/2001");
 
+        System.out.println("You were born on: " + myDate.getDate());
 
+        System.out.println("Day of the month: " + myDate.getNumberDayOfTheMonth());
+
+        System.out.println("Day of the year: " + myDate.getNumberDayOfTheYear());
+
+        System.out.println("Weekday: " + myDate.getDayOfWeek());
+
+        System.out.println("Month: " + myDate.getMonth());
+
+        System.out.println("On that day you were 100 days old: " + myDate.get100DaysOld());
+
+        System.out.println("On the day you were 10 months old: " + myDate.get10MonthsOld());
 
         System.exit(0);
     }
--- 4-Java-Libraries-part-3/Exercices/4 Start folders Java Libraries/exercise-date-analyzer/src/main/java/fact/it/exercisedateanalyzer/model/DateAnalyzer.java
+++ 4-Java-Libraries-part-3/Exercices/4 Start folders Java Libraries/exercise-date-analyzer/src/main/java/fact/it/exercisedateanalyzer/model/DateAnalyzer.java
@@ -1,19 +1,48 @@
 package fact.it.exercisedateanalyzer.model;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class DateAnalyzer {
     private LocalDate date;
 
-    public DateAnalyzer(LocalDate date) {
-        this.date = date;
+    public DateAnalyzer(String date) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("d/M/yyyy");
+        this.date = LocalDate.parse(date,dtf);
     }
 
-    public LocalDate getDate() {
-        return date;
+    public String getDate(){
+        return convertDateToString(this.date);
     }
 
     public int getNumberDayOfTheYear(){
-        return 
+        return this.date.getDayOfYear();
+    }
+
+    public int getNumberDayOfTheMonth(){
+        return this.date.getDayOfMonth();
+    }
+
+    public DayOfWeek getDayOfWeek(){
+        return this.date.getDayOfWeek();
+    }
+
+    public Month getMonth(){
+        return this.date.getMonth();
+    }
+
+    public String get100DaysOld(){
+        return convertDateToString(this.date.plusDays(100));
+    }
+
+    public String get10MonthsOld(){
+        return convertDateToString(this.date.plusMonths(10));
+    }
+
+    private String convertDateToString(LocalDate dateToConvert){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("d-M-yyyy");
+        return dateToConvert.format(dtf);
     }
 }
